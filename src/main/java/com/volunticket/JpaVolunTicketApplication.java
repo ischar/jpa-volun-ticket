c61e4b90f4233ec6d08ef9ab991a93181db3fa6c
Jaeho Cha <ischar@naver.com>
2024-11-22 13:34:02 +0900

    feat: setup project

---
+++ src/main/java/com/volunticket/JpaVolunTicketApplication.java
@@ -0,0 +1,13 @@
+package com.volunticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaVolunTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaVolunTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volunticket/JpaVolunTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaVolunTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
